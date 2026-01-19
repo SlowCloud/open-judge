c3651298f55b6d9a85e5dc97170c41cf5200b9e6
slowcloud <sju9417@gmail.com>
2026-01-19 20:42:10 +0900

    입력이 있는 코드 실행 테스트 추가

--- src/runner_test.go
+++ src/runner_test.go
@@ -24,3 +24,20 @@ func TestRun_Fail(t *testing.T) {
 		}
 	}
 }
+
+func TestRunWithInput(t *testing.T) {
+	for _, runner := range runners {
+		input := "this_is_input!"
+		res, err := runner.RunWithInput(interfaces.Input(input), `package main; import "fmt"; func main() {
+		var s string
+		fmt.Scan(&s)
+		fmt.Print(s)
+		}`)
+		if err != nil {
+			t.Fatal("failed to run code!", err)
+		}
+		if res.GetOutput() != input {
+			t.Fatal("output is not same with expected! ", res.GetOutput())
+		}
+	}
+}
