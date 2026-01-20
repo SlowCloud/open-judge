ba11eaa324e8ec47bf930582fba768a51e3f578b
slowcloud <sju9417@gmail.com>
2026-01-20 11:47:12 +0900

    runner 인터페이스 변경

--- runner/runner.go
+++ runner/runner.go
@@ -3,6 +3,6 @@ package runner
 import "open-judge/core"
 
 type Runner interface {
-	Run(code core.Code) (Result, error)
-	RunWithInput(input core.Input, code core.Code) (Result, error)
+	Run(code core.Code, timeout int) (Result, error)
+	RunWithInput(input core.Input, code core.Code, timeout int) (Result, error)
 }
