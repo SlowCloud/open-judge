6ac047d8f2f5f172e52f97374c73abe79414a475
slowcloud <sju9417@gmail.com>
2026-01-20 12:07:34 +0900

    Judge 메소드 설명 추가

--- judge/judge.go
+++ judge/judge.go
@@ -7,5 +7,7 @@ import (
 
 type Judge interface {
 	setRunner(runner runner.Runner)
-	Judge(problem core.Problem, code core.Code) (bool, error)
+
+	// 컴파일/런타임 오류 발생 시 에러 반환. result에선 정답 여부 반환
+	Judge(problem core.Problem, code core.Code) (result bool, err error)
 }
