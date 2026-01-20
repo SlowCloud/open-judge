0888953c05bae351f8e1e394e9d466a988208a9c
slowcloud <sju9417@gmail.com>
2026-01-20 12:20:06 +0900

    Judge 인터페이스의 불필요한 메소드 제거

--- judge/judge.go
+++ judge/judge.go
@@ -2,12 +2,9 @@ package judge
 
 import (
 	"open-judge/core"
-	"open-judge/runner"
 )
 
 type Judge interface {
-	setRunner(runner runner.Runner)
-
 	// 컴파일/런타임 오류 발생 시 에러 반환. result에선 정답 여부 반환
 	Judge(problem core.Problem, code string) (result bool, err error)
 }
