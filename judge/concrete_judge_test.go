b7fa734d0f63446d371a9feffddd4b7ccc71ed1d
slowcloud <sju9417@gmail.com>
2026-01-21 10:50:08 +0900

    저지 테스트코드 주석 처리
    - 로컬러너로 재구성 필요

--- judge/concrete_judge_test.go
+++ judge/concrete_judge_test.go
@@ -1,35 +1,28 @@
 package judge_test
 
-import (
-	"open-judge/core"
-	"open-judge/judge"
-	"open-judge/runner"
-	"testing"
-)
+// var (
+// 	defaultLimit = core.Limit{
+// 		TimeLimit:   1000,
+// 		MemoryLimit: 5 * 1000 * 1000, // 5MB
+// 	}
+// 	mockProblem = core.Problem{
+// 		TestCases: []core.TestCase{
+// 			{
+// 				Input:  "",
+// 				Answer: runner.MockAnswer,
+// 			},
+// 		},
+// 		Limit: defaultLimit,
+// 	}
+// )
 
-var (
-	defaultLimit = core.Limit{
-		TimeLimit:   1,
-		MemoryLimit: 5 * 1000 * 1000, // 5MB
-	}
-	mockProblem = core.Problem{
-		TestCases: []core.TestCase{
-			{
-				Input:  "",
-				Answer: runner.MockAnswer,
-			},
-		},
-		Limit: defaultLimit,
-	}
-)
-
-func TestJudge(t *testing.T) {
-	judge := judge.New(runner.MockRunner{})
-	res, err := judge.Judge(mockProblem, runner.MockCode)
-	if err != nil {
-		t.Fatal("failed to judge!", err)
-	}
-	if !res {
-		t.Fatal("answer is not correct!")
-	}
-}
+// func TestJudge(t *testing.T) {
+// 	judge := judge.New(runner.MockRunner{})
+// 	res, err := judge.Judge(mockProblem, runner.MockCode)
+// 	if err != nil {
+// 		t.Fatal("failed to judge!", err)
+// 	}
+// 	if !res {
+// 		t.Fatal("answer is not correct!")
+// 	}
+// }
